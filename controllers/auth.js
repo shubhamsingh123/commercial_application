18dcb2be92403bafc58a87fd31a04f7a55a3d4e0
Shubham Singh <Shubham16cse06@gmail.com>
2021-03-15 12:49:08 +0530

    Saving a user to database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,19 @@
+const User = require("../models/user");
+
 exports.signup = (req, res) => {
-  console.log("REQ BODY:", req.body);
-  res.json({
-    message: "signup routes works",
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "NOT ABLE TO SAVE USER IN DB",
+      });
+    } else {
+      res.json({
+        name: user.name,
+        email: user.email,
+        id: user._id,
+      });
+    }
   });
 };
 
