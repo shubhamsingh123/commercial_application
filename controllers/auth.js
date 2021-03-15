0c2fd45444f6d2a32f2861531751b7370880a524
Shubham Singh <Shubham16cse06@gmail.com>
2021-03-15 12:14:05 +0530

    How to use controllers

---
+++ controllers/auth.js
@@ -0,0 +1,6 @@
+exports.signout = (req, res) => {
+  // res.send("user signout");
+  res.json({
+    message: "User signout",
+  });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,8 @@ const { Router } = require("express");
 var express = require("express");
 var router = express.Router();
 
-router.get("/signout", (req, res) => {
-  res.send("user signout");
-});
+const { signout } = require("../controllers/auth");
+
+router.get("/signout", signout);
 
 module.exports = router;
