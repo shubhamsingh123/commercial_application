61e5750b1e0a96e6bb0f5fc1278751c6bc5ca9c2
Shubham Singh <Shubham16cse06@gmail.com>
2021-06-10 23:37:25 +0530

    Environment variable in process

--- app.js
+++ app.js
@@ -3,21 +3,20 @@
 // 404-> Not Found
 // 401-> Unauthorized
 // 500-> Internal Server Error
+require('dotenv').config();
 
 const express = require('express');
-
 const mongoose = require('mongoose');
-
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+mongoose.connect(process.env.DATABASE , {useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
 	console.log('DB Connected')
 })
 
 
 
 // port number
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port , ()=>{
 	console.log(`The server is running on ${port}`)
 })
